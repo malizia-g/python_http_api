7d11de3dc85c2c691bf2e274a1cb3767abac91bd
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-07-23 16:11:24 +0000

    aggiunta funzione ci_vettore con parametro foglio

--- main.py
+++ main.py
@@ -25,9 +25,33 @@ def get_all_addresses():
         output.append(s['INDIRIZZO'] + "|" + s['CI_VETTORE'])
     return jsonify({'result': output})
 
+
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore(foglio):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "FOGLIO" : foglio
+    }
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE']
+        }
+        )
+    return jsonify({'result': output})
+
 # Estrae consumo energetico medio e totale raggruppato per sezione catastale (e Poligono WKT)
-@app.route('/avgs', methods=['GET'])
-def get_all_stars():
+
+
+@app.route('/avgs/<float:lng>/<float:lat>/<float:r>', methods=['GET'])
+def get_avg(lng, lat, r):
+    print(type(lng))
     mil4326WKT = mongo.db.MilWKT4326
     output = []
 
@@ -35,6 +59,22 @@ def get_all_stars():
         '$match': {
             'EP_H_ND': {
                 '$gt': 0
+            },
+            'WGS84_X':
+            {
+                '$gt': lng -r
+            },
+            'WGS84_X':
+            {
+                '$lt': lng + r
+            },
+            'WGS84_Y':
+            {
+                '$gt': lat - r
+            },
+            'WGS84_Y':
+            {
+                '$lt': lat + r
             }
         }
     }
@@ -53,14 +93,15 @@ def get_all_stars():
         }
     }
     limit = {
-        '$limit' : 10
+        '$limit': 10
     }
-    
+
     for s in mil4326WKT.aggregate([match, group, limit]):
-        g1= shapely.wkt.loads(s['_id']['WKT']) #Converte da WKT in GeoJson Geometry
-        g2 = geojson.Feature(geometry=g1, 
-        properties={'id':s['_id']['SEZ'], 'media':s['AVG'], 'somma':s['SUM'], 'sezione':s['_id']['SEZ']}) 
-        output.append(g2)                 
+        # Converte da WKT in GeoJson Geometry
+        g1 = shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1,
+                             properties={'id': s['_id']['SEZ'], 'media': s['AVG'], 'somma': s['SUM'], 'sezione': s['_id']['SEZ']})
+        output.append(g2)
     return jsonify({'result': output})
 
 
@@ -69,6 +110,7 @@ def get_all_stars():
 # Generic Python functino that returns "Hello world!"
 def index():
     return "Hello world!"
+
 
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
