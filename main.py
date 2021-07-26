b544d7566ccc91c0a87e7f2d5259ea06887fd9c8
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-07-26 15:46:04 +0000

    server python terminato

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ app.config["MONGO_URI"] = "mongodb+srv://studente:studente@relab.rytr6.mongodb.n
 
 mongo = PyMongo(app)
 
-#Per rispondere alle chiamate cross origin
+# Per rispondere alle chiamate cross origin
 CORS(app)
+
 
 @app.route('/addresses', methods=['GET'])
 def get_all_addresses():
@@ -34,21 +35,93 @@ def get_vettore(foglio):
     mil4326WKT = mongo.db.MilWKT4326
     output = []
     query = {
-        "FOGLIO" : foglio
+        "FOGLIO": foglio
     }
     for s in mil4326WKT.find(query):
         output.append({
-            "INDIRIZZO":s['INDIRIZZO'],
-            "WGS84_X":s["WGS84_X"],
-            "WGS84_Y":s["WGS84_Y"],
-            "CLASSE_ENE":s["CLASSE_ENE"],
-            "EP_H_ND":s["EP_H_ND"],
-            "FOGLIO":s["FOGLIO"],
-            "CI_VETTORE":s['CI_VETTORE']
+            "INDIRIZZO": s['INDIRIZZO'],
+            "WGS84_X": s["WGS84_X"],
+            "WGS84_Y": s["WGS84_Y"],
+            "CLASSE_ENE": s["CLASSE_ENE"],
+            "EP_H_ND": s["EP_H_ND"],
+            "FOGLIO": s["FOGLIO"],
+            "CI_VETTORE": s['CI_VETTORE']
         }
         )
     return jsonify(output)
 
+
+@app.route('/ci_geovettore/<float:lng>/<float:lat>/<float:r>', methods=['GET'])
+def get_vettore_coord(lng, lat, r):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        '$and':
+        [
+            {'WGS84_X': {'$gt': lng - r}},
+            {'WGS84_X': {'$lt': lng + r}},
+            {'WGS84_Y': {'$gt': lat - r}},
+            {'WGS84_Y': {'$lt': lat + r}}
+        ]
+    }
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO": s['INDIRIZZO'],
+            "WGS84_X": s["WGS84_X"],
+            "WGS84_Y": s["WGS84_Y"],
+            "CLASSE_ENE": s["CLASSE_ENE"],
+            "EP_H_ND": s["EP_H_ND"],
+            "FOGLIO": s["FOGLIO"],
+            "CI_VETTORE": s['CI_VETTORE']
+        }
+        )
+    return jsonify(output)
+
+
+# Estrae consumo energetico medio e totale raggruppato per sezione catastale (e Poligono WKT)
+@app.route('/geogeom/<float:lng>/<float:lat>/<float:r>', methods=['GET'])
+def get_avg(lng, lat, r):
+    print(type(lng))
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    match = {
+        '$match': {
+            '$and':
+            [
+                {'EP_H_ND': {'$gt': 0}},
+                {'WGS84_X': {'$gt': lng - r}},
+                {'WGS84_X': {'$lt': lng + r}},
+                {'WGS84_Y': {'$gt': lat - r}},
+                {'WGS84_Y': {'$lt': lat + r}}
+            ]
+        }
+    }
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+    limit = {
+        '$limit': 10
+    }
+
+    for s in mil4326WKT.aggregate([match, group, limit]):
+        # Converte da WKT in GeoJson Geometry
+        g1 = shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1,
+                             properties={'id': s['_id']['SEZ'], 'media': s['AVG'], 'somma': s['SUM'], 'sezione': s['_id']['SEZ']})
+        output.append(g2)
+    return jsonify(geojson.FeatureCollection(output))
 
 
 # Annotation that allows the function to be hit at the specific URL.
